d2ccd32785f689cfbb79fc87cf596d1fb0f91f80
Kyra Abbu <abbukyra@gmail.com>
2018-09-06 09:00:44 -0400

    Adding make_abba and hello_name

--- lab_01/program1.py
+++ lab_01/program1.py
@@ -3,3 +3,17 @@ def greeter(name):
     return "Hello " +name+ "!"
 
 print(greeter("Stan"))
+
+#String-1>hello_name
+def hello_name(name):
+  return "Hello "+name+"!"
+
+print(hello_name("Sam"))
+
+#String-1>make_abba
+def make_abba(a, b):
+  return a+b+b+a
+
+print(make_abba("Hi","Bye"))
+print(make_abba("Yo","Alice"))
+print(make_abba("What","Up"))
