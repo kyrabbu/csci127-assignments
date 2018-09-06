40cc6d03381bc16c326c8fcf7b9886a8f9a41be1
Kyra Abbu <abbukyra@gmail.com>
2018-09-06 09:18:14 -0400

    Adding warm_up sleep_in using t/f

--- lab_01/program1.py
+++ lab_01/program1.py
@@ -23,3 +23,15 @@ def make_tags(tag, word):
   return "<"+tag+">"+word+"</"+tag+">"
 
 print(make_tags("i","Yay"))
+
+#Warmup-1>sleep_in
+def sleep_in(weekday, vacation):
+  if not weekday or vacation: #or means either one or both weekday or vacation 
+    return True
+  else:
+    return False
+
+print(sleep_in(False,True))
+print(sleep_in(False,False))
+print(sleep_in(True,True))
+print(sleep_in(True,False))
