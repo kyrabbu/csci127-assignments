da5674757bcdc4cb68bbf7bf4c2ee58414ec9953
Kyra Abbu <abbukyra@gmail.com>
2018-09-24 08:29:08 -0400

    added comments

--- lab_02/fizzbuzz.py
+++ lab_02/fizzbuzz.py
@@ -12,16 +12,16 @@ def fizzbuzz(max_value):
     count = 0
     i = 1
     while i <= max_value:
-        if (i % 5 == 0) and (i % 3 == 0): #must go first so that 3 and 5 do not repeat 
-            print("FizzBuzz") #if divisible by both
-            count +=1 #counts how many times FizzBuzz occurs
-        elif i % 3 == 0: #must use elif so it scans this after "if"
-            print("Fizz") #if divisible by 3
+        if (i % 5 == 0) and (i % 3 == 0): #checks if divisible by both
+            count += 1 #counts how many times FizzBuzz occurs
+            print("FizzBuzz")  
+        elif i % 3 == 0: #must use elif so it scans this after "if"; checks if divisible by 3
+            print("Fizz") 
         elif i % 5 == 0: 
-            print("Buzz") #if divisible by 5
+            print("Buzz") #checks if divisible by 5
         else: #if not divisible by anything
             print(i)
-        i = i + 1 #loops until it reaches 100
-    return print("Count:", count) #returns value of count
+        i = i + 1 #loops until it reaches max_value
+    return "Count:" + str(count) #turns count into string so it concantenates with string
 
-fizzbuzz(100)
+print(fizzbuzz(100)) #print prints return value, without print count will not appear
