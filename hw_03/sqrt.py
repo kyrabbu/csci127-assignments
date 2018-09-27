b2850e3fddb0f32c9aca8f9641b76afdf169aed2
Kyssa Abbu <kyssabbu@Kyssas-MacBook-Pro.local>
2018-09-27 11:56:54 -0400

    added sqrt.py

---
+++ hw_03/sqrt.py
@@ -0,0 +1,20 @@
+#Kyra and Will
+from random import randint  #import to generate random integer
+
+def mysqrt(number):
+    print("Guess the square root of", number,":")
+    guess = randint(0, number) #generates random number
+    previousGuess = 0 #stores value under while
+    numberOfGuesses = 0 #counts loop times
+    while previousGuess != guess:
+        print(guess)
+        previousGuess = guess #replaces value of initial previousGuess
+        guess = (guess + number/guess) / 2       
+        numberOfGuesses+=1
+    print("Number of guesses: ", numberOfGuesses)
+
+
+mysqrt(1000)
+mysqrt(9)
+mysqrt(16)
+mysqrt(10000)
