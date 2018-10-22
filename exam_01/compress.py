e6df495982ecfad31818f05eb642cf0c7cc695e2
Kyra Alyssa <abbukyra@gmail.com>
2018-10-22 09:06:54 -0400

    added .lower()

--- exam_01/compress.py
+++ exam_01/compress.py
@@ -7,13 +7,15 @@ def compress_word(w):
     newl.append(w[0].lower())
     while i < len(w):
         if w[i] not in vowel:
-            newl.append(w[i])
+            value = w[i].lower()
+            newl.append(value)
         i = i + 1
     print(''.join(newl))
             
 compress_word("halloween") #hllwn
 compress_word("Special") #spcl
 compress_word("apple") #appl
+compress_word("oRaNge") #orng
 
 def sentence(line):
     x = compress_word(line)
