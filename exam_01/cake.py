4cf2dd78f292cea381f1b5a4bfa7c93f5f568202
Kyra Alyssa <abbukyra@gmail.com>
2018-10-22 09:01:36 -0400

    added cake

---
+++ exam_01/cake.py
@@ -0,0 +1,10 @@
+#Kyra Abbu
+
+def divide(A,B,u):
+    party_size = u//(A/B)
+    return party_size
+
+print("Party Size:",divide(5,10,1))
+print("Party Size:",divide(1,2,9))
+print("Party Size:",divide(3,1,6))
+print("Party Size:",divide(1,8,9))
