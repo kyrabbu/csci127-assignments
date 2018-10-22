e088030c4125349089837ff0fe310ba497d7859d
Kyra Alyssa <abbukyra@gmail.com>
2018-10-22 08:48:17 -0400

    added compress.py

---
+++ exam_01/compress.py
@@ -0,0 +1,25 @@
+#Kyra Abbu
+
+def compress_word(w):
+    i = 1
+    newl = []
+    vowel = "AEIOUaeiou"
+    newl.append(w[0].lower())
+    print(w)
+    while i < len(w):
+        if w[i] not in vowel:
+            newl.append(w[i])
+        i = i + 1
+    print(''.join(newl))
+            
+compress_word("halloween") #hllwn
+compress_word("Special") #spcl
+compress_word("apple") #appl
+
+def sentence(line):
+    x = compress_word(line)
+    return x
+    
+sentence("I like to eat apple pie.") #i lk t t ppl p
+sentence("Who is there")
+    
