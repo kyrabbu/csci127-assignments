dd680a28cbe4af84b9648a5178d05354c3e42714
Kyra Alyssa <abbukyra@gmail.com>
2018-10-22 09:10:34 -0400

    added function

--- exam_01/cake.py
+++ exam_01/cake.py
@@ -4,7 +4,7 @@ def divide(A,B,u):
     party_size = u//(A/B)
     return party_size
 
-print(divide(5,10,1))
-print(divide(1,2,9))
-print(divide(3,1,6))
-print(divide(1,8,9))
+print(int(divide(5,10,1)))
+print(int(divide(1,2,9)))
+print(int(divide(3,1,6)))
+print(int(divide(1,8,9)))
