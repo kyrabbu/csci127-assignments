6d3c38fd4ab898208d36b34b273ab83ad0e02976
Kyra Alyssa <abbukyra@gmail.com>
2018-11-19 15:16:02 -0500

    added comments

--- exam_02/rle.py
+++ exam_02/rle.py
@@ -1,57 +1,35 @@
 def encode(string):
     l = []
-    d = {}
-    alphabet = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm"
-    for i in string:
-        if i in alphabet:
-            d.setdefault(i,0)
-            d[i] = d[i] + 1
-    for k,v in d.items():
-        list = []
-        list.append(k)
-        list.append(v)
-        l.append(list)
-        #print(list)
+    count = 1
+    for i in range(len(string)):
+        #print(i)
+        if  i == len(string)-1 or string[i] != string[i+1]:
+            l.append([string[i], count])
+            count = 1
+        else:
+            count = count + 1
     return l
 
 def decode(list):
-    d = {}
     result = ""
     for item in list:
-        d.setdefault(item[0],0)
-        d[item[0]] = item[1]
-    for k in d:
-        repeat = k * d[k]
-        result = result + repeat
+        result = result + item[0]*item[1]
     return result
 
-#tests
-print("Program will return empty list if no letters are found.")
+#print("Program will return empty list if no letters are found.")
 print(encode("abbaaacddaaa"))
 print(encode("abcd"))
 print(encode("cbbbbbdee"))
-print(encode(" "))
-print(encode("__"))
+#print(encode(" "))
+#print(encode("__"))
 print(encode("AbCdEfCoNd"))
 
-print("\nProgram will not return anything if input does not contain letters OR is not a letter.")
+#print("\nProgram will not return anything if input does not contain letters OR is not a letter.")
 print(decode(encode("abbaaacddaaa")))
 print(decode(encode("abcd")))
 print(decode(encode("cbbbbbdee")))
-print(decode(encode(" ")))
-print(decode(encode("__")))
+#print(decode(encode(" ")))
+#print(decode(encode("__")))
 print(decode(encode("AbCdEfCoNd")))
-print(decode(encode("hell0")))
+#print(decode(encode("hell0")))
 
-
-
-
-
-        
-
-
-
-
-
-
-        
