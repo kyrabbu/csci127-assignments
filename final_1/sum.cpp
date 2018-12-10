c3a54c37187205d99ea6a16fbc035a46227e1f7a
Kyra Alyssa <abbukyra@gmail.com>
2018-12-10 09:02:44 -0500

    minor changes

--- final_1/sum.cpp
+++ final_1/sum.cpp
@@ -4,18 +4,21 @@
 int sumofsquares(int a, int b)
 {
   int i;
+  int summedup=0;
   if (a<b)
   {
     for (i = a; i <= b; i++)
     {
       int squared = pow(i,2);
-      std::cout << squared << std::endl;
+      summedup = summedup + squared;
+      //std::cout << squared << std::endl;
     }
   }
   else
   {
     std::cout << "your input is invalid: a>b" << std::endl;
   }
+  std::cout << summedup << std::endl;
   return 0;
 }
 int main()
