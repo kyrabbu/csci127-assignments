5e63b20a9d590f008258b32bd3e6f302c178a872
Kyra Alyssa <abbukyra@gmail.com>
2018-12-20 10:24:11 -0500

    k

---
+++ final_2/dict.py
@@ -0,0 +1,26 @@
+def addline(d, line):
+    l = line.lower() #lower case
+    for element in l.split():
+        d.setdefault(element[0], []) #make first letter key
+        #makes list as value
+        d[element[0]] = d[element[0]] + [element]
+    return d
+
+def spellcheck(d, word):
+    w = word.lower()
+    for l in d.values():
+        if w in l:
+            return True
+    return False
+
+d = {} #empty at first
+print(addline(d,"HELLO MY NAME IS KYRA"))
+print(addline(d,"I LIKE APPLES"))
+print(addline(d,"HeLlO mY nAmE is KyRa"))
+print(addline(d,"WHAT is WRONG with THIS line"))
+
+k = addline(d,"HELLO MY NAME IS KYRA")
+print(spellcheck(k,"DO YOU LIKE APPLES"))
+print(spellcheck(k,"hi"))
+print(spellcheck(k,"i"))
+print(spellcheck(k,"kyra"))
