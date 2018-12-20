812d26c9b651b3f0dc7ba2c6a976d7d9bedfb2dc
Kyra Alyssa <abbukyra@gmail.com>
2018-12-20 10:30:28 -0500

    change is good

--- final_2/plural.py
+++ final_2/plural.py
@@ -14,5 +14,8 @@ def notPossessive(line):
 
 #tests
 print("Plurals:", countPlurals("apples oranges grape banana"))
+print("Plurals:", countPlurals("apples apples grapes banana"))
+print("Plurals with 's:", countPlurals("apple's orange's grape's banana's"))
+print("Plurals with 's:", countPlurals("apple'ss orange's grape's banana's"))
 print("No Possessives:", notPossessive("apple's oranges grape's banana"))
 print("No Possessives:", notPossessive("apples oranges grapes banana"))
